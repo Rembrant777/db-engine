9742e1847f5560b7df4d57bf82f3ad4e50f80fac
nanachi1027 <nanachi1027@163.com>
2023-02-15 17:17:56 +0800

    update

---
+++ cmake-repo/01-basic/F-build-type/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by nanachi on 2023/2/15.
+//
+#include <iostream>
+using namespace std;
+int main(int argc, char * argv []) {
+    cout << "cmake example with build type flags" << endl;
+    return 0;
+}
