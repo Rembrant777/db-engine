b477ec707e0341b87f63c1306113f0178f923c25
nanachi1027 <nanachi1027@163.com>
2023-03-08 12:39:39 +0800

    update

---
+++ server_main.cpp
@@ -0,0 +1,31 @@
+/*    Copyright 2009 10gen Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+*/
+
+#include <iostream>
+
+#include "core.hpp"
+#include "ossSocket.hpp"
+#include "client/command.hpp"
+#include "client/commandFactory.hpp"
+#include "client/edb.hpp"
+
+using namespace std; 
+using namespace emeralddb; 
+
+// entry point for server driver 
+int main() {
+    cout << "hello world from server side" << endl; 
+    return 0; 
+}
